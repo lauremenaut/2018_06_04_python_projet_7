e2097b0bf8f66ce651c281a350d5df5d34c424cb
Laure <lauremenaut@yahoo.fr>
2018-06-07 16:21:19 -0400

    Created test_gmaps_API_request.py file

---
+++ test_gmaps_API_request.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+from gmaps_API_request import GmapsApiRequest
+
+
+class TestGmapsApiRequest:
+    gmaps_API_request = GmapsApiRequest()
+
+    def test_(self):
+        assert
