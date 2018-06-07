734ce7a84eab4c76a8be401cbbc16251d21d9a0f
Laure <lauremenaut@yahoo.fr>
2018-06-07 16:23:39 -0400

    Imported 2 API modules & added code involving them in __init__() method

--- app.py
+++ app.py
@@ -3,13 +3,16 @@
 
 """
 Exemples de questions posées par l'utilisateur :
-- Où se trouve la Tour Eiffel ?
+- Où se trouve l'Arc de triomphe à Paris ?
 - Salut vieux robot, quelle est l'adresse du cinéma le plus proche ?
 - Bonjour GrandPy, comment vas-tu ? J'aimerais savoir comment aller à la FNAC de Toulouse.
-
+- Quelle est l'adresse de la piscine de Foix ?
 """
 
 import json
+
+from gmaps_API_request import GmapsApiRequest
+from mediawiki_wrapper import MediaWikiWrapper
 
 
 class App:
@@ -26,25 +29,34 @@ class App:
         Manages ...
 
         """
-        with open("stop_words.json", 'r') as f:
-            self.stop_words = json.load(f)
+        query = self.parser()
+        gmaps_api_request = GmapsApiRequest(query)
+        address = gmaps_api_request.address
+        lat = gmaps_api_request.lat
+        lng = gmaps_api_request.lng
+        print("\nTu trouveras ça au", address)
+        mediawiki_wrapper = MediaWikiWrapper(lat, lng)
+        print("\nEn parlant de ça, savais-tu que ")
+
+        summary = mediawiki_wrapper.summary
+        print(summary)
 
     def parser(self):
         user_query = self.get_user_query()
-        print("User query : ", user_query)
+        # print("User query : ", user_query)
         sentences = self.cut_into_sentences(user_query)
-        print("Sentences : ", sentences)
+        # print("Sentences : ", sentences)
         chosen_sentence = self.choose_sentence(sentences)
-        print("Chosen sentences : ", chosen_sentence)
+        # print("Chosen sentences : ", chosen_sentence)
         words = self.cut_chosen_sentence(chosen_sentence[-1])  # Au cas où il y ait plusieurs phrases sélectionnées, on choisit la dernière.
-        print("Words : ", words)
+        # print("Words : ", words)
         filtered_words = self.filter_words(words)
-        print("Filtered words : ", filtered_words)
+        # print("Filtered words : ", filtered_words)
+        return filtered_words
 
     def get_user_query(self):
-        user_query = input('Bonjour mon petit, quelle est ta question ?\n')
+        user_query = input('\nBonjour mon petit, quelle est ta question ?\n\n')
         user_query_lower = user_query.lower()
-        # user_query = 'Où se trouve la Tour Eiffel ?'
         return user_query_lower
 
     def cut_into_sentences(self, user_query):
@@ -65,16 +77,18 @@ class App:
         return words
 
     def filter_words(self, words):
+        with open("stop_words.json", 'r') as f:
+            stop_words = json.load(f)
+
         filtered_words = []
         for word in words:
-            if word not in self.stop_words:
+            if word not in stop_words:
                 filtered_words.append(word)
         return filtered_words
 
-def main():
-    app = App()
-    app.parser()
 
+def main():
+    App()
 
 if __name__ == '__main__':
     main()
