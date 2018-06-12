ba1a67fd357721025506fef00071034b96208ee1
Laure <lauremenaut@yahoo.fr>
2018-06-11 23:06:27 -0400

    Added EventListener (doesn't work yet ...)

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,1 +1,15 @@
-var dialogueAreaElt = document.getElementById("dialogue_area")
+var dialogueAreaElt = document.getElementById("dialogue_area");
+var queryFieldElt = document.getElementById("query_field");
+
+document.addEventListener("keypress", function(e) {
+    console.log("Appuyé sur : " + e.charCode);
+    if (e.charCode == 108) {
+        console.log("Bien appuyé sur le 'l' !");
+        console.log("queryFieldElt.value : " + queryFieldElt.value);
+        var pElt = document.createElement("p");
+        var form = document.querySelector("form");
+        pElt.textContent = form.elements[0].value;
+        console.log("pElt.textContent : " + pElt.textContent);
+        dialogueAreaElt.appendChild(pElt);
+    }
+});
