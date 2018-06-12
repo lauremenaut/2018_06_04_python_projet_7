5a6fb3fa3fa950cbb6ae87720779ec9ae32aeefc
Laure <lauremenaut@yahoo.fr>
2018-06-11 23:20:16 -0400

    Changed .elements[0]• into .elements[1]• (now working fine =)

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -3,13 +3,11 @@ var queryFieldElt = document.getElementById("query_field");
 
 document.addEventListener("keypress", function(e) {
     console.log("Appuyé sur : " + e.charCode);
-    if (e.charCode == 108) {
+    if (e.charCode == 0) {
         console.log("Bien appuyé sur le 'l' !");
-        console.log("queryFieldElt.value : " + queryFieldElt.value);
         var pElt = document.createElement("p");
         var form = document.querySelector("form");
-        pElt.textContent = form.elements[0].value;
-        console.log("pElt.textContent : " + pElt.textContent);
+        pElt.textContent = form.elements[1].value;
         dialogueAreaElt.appendChild(pElt);
     }
 });
