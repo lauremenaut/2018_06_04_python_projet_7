f2d603df92e6ff729ff6accc416a376d03a42d58
Laure <lauremenaut@yahoo.fr>
2018-06-18 23:53:39 -0400

    Moved 'var form' & 'var query' towards EventListenener - Added .value =  in EventListener - Added 'loading.gif' & 'initMap' code in locate () function - Added initMap() function

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,37 +1,62 @@
-$('#dialogue_area').append("<br>Bonjour mon petit, as-tu une question pour moi ?<br>")
-
-var form = document.querySelector("form"); // Champ de saisie
-var query = form.elements[1].value; // Contenu du champ de saisie (= saisie de l'utilisateur)
+$('#dialogue_area').append('<br>GrandPy : Bonjour mon petit, as-tu une question pour moi ?<br>');
 
 
-function locate(sourceElt, destElt) {
-// Ajouter le .gif quand GrandPy réfléchit
+function initMap(lat, lng) {
+        var place = {lat: lat, lng: lng};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 17,
+          center: place
+        });
+        var marker = new google.maps.Marker({
+          position: place,
+          map: map
+        });
+      }
+
+function locate(query, dialogue_area) {
+    // Ajout de l'animation pendant que "GrandPy réfléchit"
+    $(dialogue_area).append("<img src='../static/img/loading_mini.gif' alt='loading' class='loading' />");
+
     $.post('/locate', {
-        // query: $(sourceElt).text() Pourquoi ça ne fonctionne pas ?
         query: query
     }).done(function(response) {
-        $(destElt).append('Oh oui, je connais ! Tu trouveras ça au ' + response['localisation'][0] + '.<br>')
-        $(destElt).append('En parlant de ça, j\'avais une petite chose à te raconter ... ' + response['localisation'][1])
+        // Retrait de l'animation
+        $('.loading').remove();
+
+        // Affichage de l'adresse
+        address = response['localisation'][0];
+        $(dialogue_area).append('GrandPy : Hum ... oh oui, je me souviens ! Tu trouveras ça au ' + address + '.<br><br>');
+
+        // Insertion de la carte
+        lat = response['localisation'][1];
+        lng = response['localisation'][2];
+        initMap(lat, lng);
+
+        // Affichage de l'anecdote
+        summary = response['localisation'][3];
+        $(dialogue_area).append('GrandPy : En parlant de ça, j\'avais une petite chose à te raconter ... ' + summary);
+        $(dialogue_area).append('<br><br>GrandPy : As-tu autre chose à me demander ?<br>');
     }).fail(function() {
-        $(destElt.text("Ça, je ne m'en souviens plus ..."))
+        $(dialogue_area.append('Ça, je ne m\'en souviens plus ...'));
     });
 }
 
 document.addEventListener("keypress", function(e) {
+
+    var form = document.querySelector("form"); // Champ de saisie
+    var query = form.elements[1].value; // Contenu du champ de saisie (= saisie de l'utilisateur)
+
     if (e.key == "Enter") {
         // On affiche la saisie de l'utilisateur dans la zone de dialogue
-        var pElt1 = document.createElement("p");
-        pElt1.textContent = query;
-        $('#dialogue_area').append(pElt1);
+        var pElt = document.createElement("p");
+        pElt.textContent = 'Vous : ' + query;
+        $('#dialogue_area').append(pElt);
 
         locate(
-            'query',
+            query,
             '#dialogue_area'
-            )
-    }
-});
+            );
 
-// Vider le champ de recherche au démarrage
-// Ajouter une boucle
-// Ajouter le nom de celui qui parle
-// Ajouter un ascenseur quand le contenu dépasse la taille de la zone de dialogue
+        form.elements[1].value = "";  // Est-ce une bonne façon de vider le champ de saisie ??
+    };
+});
