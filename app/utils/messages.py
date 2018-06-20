cd443540acacec35642b3d978f919e8c1dd6f1c7
Laure <lauremenaut@yahoo.fr>
2018-06-20 16:45:34 -0400

    Added 'summary' & 'end' messages

--- app/utils/messages.py
+++ app/utils/messages.py
@@ -5,6 +5,20 @@ success_messages = [
     "GrandPy : Aussitôt demandé, aussitôt renseigné ! Ce que tu cherches est au ",
     "GrandPy : Il me semble que ça se trouve au "
     ]
+summary_messages = [
+    "GrandPy : En parlant de ça, j\'avais une petite chose à te raconter ... ",
+    "GrandPy : A ce sujet, savais-tu cela ? ",
+    "GrandPy : D'ailleurs, cela me rappelle quelque chose ... ",
+    "GrandPy : Au fait, cela me rappelle un souvenir ! ",
+    "GrandPy : Maintenant que tu m'y fais penser ... "
+    ]
+end_messages = [
+    "GrandPy : As-tu autre chose à me demander ?",
+    "GrandPy : Puis-je encore quelque chose pour toi ?",
+    "GrandPy : Encore une question ?",
+    "GrandPy : N'hésite pas si tu as besoin d'autre chose, je suis là !",
+    "GrandPy : As-tu encore besoin de moi ?"
+    ]
 failure_messages = [
     "GrandPy : Dis-moi ça plus clairement s'il te plait !",
     "GrandPy : Je n'ai pas compris ! Que dis-tu ?",
