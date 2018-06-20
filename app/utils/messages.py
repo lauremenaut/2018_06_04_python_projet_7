6719364e42dc171be06cd87cd982cd91b641b856
Laure <lauremenaut@yahoo.fr>
2018-06-20 15:01:40 -0400

    Created 'messages.py' file

---
+++ app/utils/messages.py
@@ -0,0 +1,14 @@
+success_messages = [
+    "GrandPy : Hum ... oh oui, je me souviens ! Tu trouveras ça au ",
+    "GrandPy : Bien sûr, voici l'adresse : ",
+    "GrandPy : Dans mes souvenirs, c'est au ",
+    "GrandPy : Aussitôt demandé, aussitôt renseigné ! Ce que tu cherches est au ",
+    "GrandPy : Il me semble que ça se trouve au "
+    ]
+failure_messages = [
+    "GrandPy : Dis-moi ça plus clairement s'il te plait !",
+    "GrandPy : Je n'ai pas compris ! Que dis-tu ?",
+    "GrandPy : Ça ne me dit rien, pourrais-tu être plus clair ?",
+    "GrandPy : Pardon ? Je n'ai pas compris !",
+    "GrandPy : Répète-moi ça en bon français STP ;-) ?"
+    ]
