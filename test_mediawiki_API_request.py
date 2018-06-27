4275b7c1dbbf9dc7e8a113d875c5f774ec1db10a
Laure <lauremenaut@yahoo.fr>
2018-06-26 22:01:15 -0400

    Created test file for Mediawiki_api_request file

---
+++ test_mediawiki_API_request.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+import requests
+
+from app.utils.mediawiki_API_request import MediaWikiApiRequest
+
+
+class TestMediawikiApiRequest:
+    mediawiki_API_request = MediaWikiApiRequest(42.9600983, 1.609331)
+
+    def test_get_pageid(self, monkeypatch):
+        results = 898403
+
+        def mockreturn(request):
+            return results
+
+        monkeypatch.setattr(requests, 'get', mockreturn)
+        assert self.mediawiki_API_request.get_pageid(42.9600983, 1.609331) == results
+
+
+
+    def test_get_summary(self, monkeypatch):
+        results = "Le Festival international de films Résistances, appelé plus simplement le Festival Résistances, se déroule à Foix en Ariège (Midi-Pyrénées) début juillet et propose une programmation de plus de 100 films, allant du documentaire à la fiction. Il est un des événements culturels les plus importants de la région.\nFondé en 1997, le festival s'inscrit dans un esprit de résistance à l'image des terres sur lesquels il a grandi. Il s'est donné comme objectif de promouvoir un cinéma engagé, rarement diffusé, et proposer un nouveau regard sur le monde.\nLe festival est peu à peu devenu l'un des événements majeurs de la contre-culture cinématographique en France."
+
+        def mockreturn(request):
+            return results
+
+        monkeypatch.setattr(requests, 'get', mockreturn)
+        assert self.mediawiki_API_request.get_summary(898403) == results
+
+
+
+# class TestGmapsApiRequest:
+#     gmaps_API_request = GmapsApiRequest()
+
+#     def test_(self):
+#         pass
+
+"""
+{'batchcomplete': '',
+ 'query':
+    {'pages':
+        {'898403':
+            {'pageid': 898403,
+             'ns': 0,
+             'title': 'Festival de films Résistances',
+             'extract': "Le Festival international de films Résistances, appelé plus simplement le Festival Résistances, se déroule à Foix en Ariège (Midi-Pyrénées) début juillet et propose une programmation de plus de 100 films, allant du documentaire à la fiction. Il est un des événements culturels les plus importants de la région.\nFondé en 1997, le festival s'inscrit dans un esprit de résistance à l'image des terres sur lesquels il a grandi. Il s'est donné comme objectif de promouvoir un cinéma engagé, rarement diffusé, et proposer un nouveau regard sur le monde.\nLe festival est peu à peu devenu l'un des événements majeurs de la contre-culture cinématographique en France."
+             }
+         }
+     }
+ }
+
+"""
