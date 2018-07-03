48a7487388da0d9b49a80478738b87c3e3593e4e
Laure <lauremenaut@yahoo.fr>
2018-07-03 14:36:33 -0400

    Added logging.basicConfig()

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,11 @@ from flask import render_template, jsonify
 from app import appl
 from app.form import QueryForm
 from app.locate import locate
+import logging
+
+
+# logging.basicConfig(filename="log.log", level=logging.DEBUG, format='%(asctime)s -- %(name)s -- %(levelname)s -- %(message)s')
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s -- %(name)s -- %(levelname)s -- %(message)s')
 
 
 @appl.route('/', methods=['GET', 'POST'])
@@ -15,8 +20,13 @@ def index():
     key = appl.config['GOOGLE_MAPS_API_KEY']
     return render_template('index.html', form=form, key=key)
 
-
 @appl.route('/locate', methods=['POST'])
 def query_locate():
     form = QueryForm()
     return jsonify({'localisation': locate(form.query.data)})
+    # try:
+    #     data = jsonify({'localisation': locate(form.query.data)})
+    # except ParserError:
+    #     pass
+
+    # return data
