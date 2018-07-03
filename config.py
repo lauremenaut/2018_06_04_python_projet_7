17ff4a964a17639f8d76b267163f800596f267ee
Laure <lauremenaut@yahoo.fr>
2018-07-03 14:35:15 -0400

    Commented private data (keys)

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import os
 
 
 class Config(object):
-    SECRET_KEY = "]d{<Xh),pyrVD.QY8owh7rNmx0b"
-    GOOGLE_MAPS_API_KEY = "AIzaSyD0CWiofMMeygd25DTranGk2XgJXjGsHfs"
-    # SECRET_KEY = os.environ.get('SECRET_KEY')
-    # GOOGLE_MAPS_API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY')
+    # SECRET_KEY = "]d{<Xh),pyrVD.QY8owh7rNmx0b"
+    # GOOGLE_MAPS_API_KEY = "AIzaSyD0CWiofMMeygd25DTranGk2XgJXjGsHfs"
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    GOOGLE_MAPS_API_KEY = os.environ.get('GOOGLE_MAPS_API_KEY')
