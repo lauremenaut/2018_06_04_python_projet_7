e59e20fb77b54a4c460533bac13298438a7a93ad
Laure <lauremenaut@yahoo.fr>
2018-07-06 12:44:54 -0400

    docs : add module docstring

--- app/utils/question_words.py
+++ app/utils/question_words.py
@@ -1,1 +1,6 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+""" Set a list of words commonly found in user queries """
+
 question_words = ["?", "ou", "où", "comment", "quel", "quelle", "est-ce", "connais", "connais-tu", "connaissez", "connaissez-vous", "adresse", "aimerais", "aller"]
