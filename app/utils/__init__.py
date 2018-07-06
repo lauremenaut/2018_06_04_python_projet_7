ca79e4f1f2f36dacd2f01561c1449c1a362d1f02
Laure <lauremenaut@yahoo.fr>
2018-07-06 12:50:40 -0400

    docs : add package docstring

---
+++ app/utils/__init__.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+""" This package contains the following modules :
+
+    - parser
+    - gmaps_API_request
+    - mediawiki_API_request
+    - messages
+    - question_words
+    - stop_words
+
+ """
