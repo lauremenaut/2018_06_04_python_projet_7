b04a5882a4d0809d71640530a34f60d7b3633502
Laure <lauremenaut@yahoo.fr>
2018-07-06 12:39:19 -0400

    docs : add module & class docstrings

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
+""" Set index() & query_locate() view functions """
+
 import logging
 
 from flask import render_template, jsonify
@@ -9,7 +11,6 @@ from app import appl
 from app.form import QueryForm
 from app.locate import locate
 
-
 # logging.basicConfig(filename="log.log", level=logging.DEBUG, format='%(asctime)s -- %(name)s -- %(levelname)s -- %(message)s')
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s -- %(name)s -- %(levelname)s -- %(message)s')
 
@@ -17,6 +18,12 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s -- %(name)s -- %(le
 @appl.route('/', methods=['GET', 'POST'])
 @appl.route('/index', methods=['GET', 'POST'])
 def index():
+    """ Set index() view function.
+
+    Use QueryForm class & GOOGLE_MAPS_API_KEY from config.
+    Render template returning HTML.
+
+    """
     form = QueryForm()
     key = appl.config['GOOGLE_MAPS_API_KEY']
     return render_template('index.html', form=form, key=key)
@@ -24,5 +31,11 @@ def index():
 
 @appl.route('/locate', methods=['POST'])
 def query_locate():
+    """ Set query_locate() view function.
+
+    Use QueryForm class.
+    Return data from locate() function, in JSON format.
+
+    """
     form = QueryForm()
     return jsonify({'localisation': locate(form.query.data)})
