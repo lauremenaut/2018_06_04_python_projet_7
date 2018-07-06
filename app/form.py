f0bd1fc94b8aae041e159595b6484a747cb107f7
Laure <lauremenaut@yahoo.fr>
2018-07-06 12:36:02 -0400

    docs : add module & class docstrings

--- app/form.py
+++ app/form.py
@@ -1,10 +1,13 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
+""" Set QueryForm class (inheriting from FlaskForm) """
+
 from flask_wtf import FlaskForm
 
 from wtforms import TextAreaField
 
 
 class QueryForm(FlaskForm):
+    """ Set 'query' variable as an instance of TextAreaField """
     query = TextAreaField("Saisissez ici votre question pour GrandPy Bot :")
