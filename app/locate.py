9852468b5ef0f8fa0c3dc028c6b1b545f4a772f4
Laure <lauremenaut@yahoo.fr>
2018-07-06 12:38:43 -0400

    refactor : set 8 variables at the beginning of locate() method, avoiding code duplication (still in progress)  - docs : add module & class docstrings

--- app/locate.py
+++ app/locate.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
+""" Set locate() function """
+
 import random
 import logging
 
@@ -8,37 +10,53 @@ from app.exceptions import ParserError, GmapsApiError, MediaWikiApiError
 from app.utils.parser import Parser
 from app.utils.gmaps_API_request import GmapsApiRequest
 from app.utils.mediawiki_API_request import MediaWikiApiRequest
-from app.utils.messages import success_messages, parser_failure_messages, address_failure_messages, summary_messages, summary_failure_messages, end_messages
+from app.utils.messages import address_success_messages, parser_failure_messages, address_failure_messages, summary_success_messages, summary_failure_messages, next_question_messages
 
 
 def locate(query):
+    """ Set locate() function.
 
+    Receive a string containing user query.
+    Use Parser, GmapsApiRequest and MediaWikiApiRequest classes.
+    Return a list of 8 variables :
+    - error : a boolean, False if an address is found, True else
+    - message : a string randomly chosen in messages module
+    - address : a string sent back by Google Maps API, or None
+    - lat : a floating number sent back by Google Maps API, or None
+    - lng : a floating number sent back by Google Maps API, or None
+    - summary_message : a string randomly chosen in messages module, or None
+    - summary : a string sent back by MediaWiki API, or None
+    - next_question_message : a string randomly chosen in messages module, or None
+
+
+    """
+    # 'error' variable is True only if no address is returned (parser failure & address failure)
     error = False
-    message = random.choice(success_messages)
-    summary_message = random.choice(summary_messages)
-    end_message = random.choice(end_messages)
+    message = random.choice(address_success_messages)
+    address = None
+    lat = None
+    lng = None
+    summary_message = random.choice(summary_success_messages)
+    summary = None
+    next_question_message = random.choice(next_question_messages)
 
     try:
         parser = Parser(query)
-        logging.debug("Here are relevant words selected by parser : {}".format(parser.query))
+        logging.debug("Here are relevant words selected by parser : {}".format(parser.query_relevant_words))
 
-        if not parser.query:
+        if not parser.query_relevant_words:
             raise ParserError("Parser didn't find any relevant word ...")
 
     except ParserError as e:
         logging.warning("ParserError : {}".format(e))
         error = True
         message = random.choice(parser_failure_messages)
-        address = None
-        lat = None
-        lng = None
         summary_message = None
-        summary = None
-        end_message = None
-        return error, message, address, lat, lng, summary_message, summary, end_message
+        next_question_message = None
+        return error, message, address, lat, lng, summary_message, summary, next_question_message
 
     try:
-        gmaps_api_request = GmapsApiRequest(parser.query)
+        gmaps_api_request = GmapsApiRequest(parser.query_relevant_words)
         address = gmaps_api_request.address
         lat = gmaps_api_request.lat
         lng = gmaps_api_request.lng
@@ -51,25 +69,17 @@ def locate(query):
         logging.warning("GmapsApiError  {}".format(e))
         error = True
         message = random.choice(address_failure_messages)
-        address = None
-        lat = None
-        lng = None
         summary_message = None
-        summary = None
-        end_message = None
-        return error, message, address, lat, lng, summary_message, summary, end_message
+        next_question_message = None
+        return error, message, address, lat, lng, summary_message, summary, next_question_message
 
     except AttributeError as e:
         logging.warning("AttributeError : {}".format(e))
         error = True
         message = random.choice(address_failure_messages)
-        address = None
-        lat = None
-        lng = None
         summary_message = None
-        summary = None
-        end_message = None
-        return error, message, address, lat, lng, summary_message, summary, end_message
+        next_question_message = None
+        return error, message, address, lat, lng, summary_message, summary, next_question_message
 
     try:
         mediawiki_api_request = MediaWikiApiRequest(lat, lng)
@@ -81,11 +91,9 @@ def locate(query):
     except MediaWikiApiError as e:
         logging.warning("MediaWikiError : {}".format(e))
         summary_message = random.choice(summary_failure_messages)
-        summary = None
 
     except AttributeError as e:
         logging.warning("AttributeError : {}".format(e))
         summary_message = random.choice(summary_failure_messages)
-        summary = None
 
-    return error, message, address, lat, lng, summary_message, summary, end_message
+    return error, message, address, lat, lng, summary_message, summary, next_question_message
