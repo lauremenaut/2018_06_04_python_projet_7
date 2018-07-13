2eb11acfd86c14561470013227c9da418b179da6
Laure <lauremenaut@yahoo.fr>
2018-07-13 00:46:53 -0400

    test : fix major issues in mock

--- tests/test_gmaps_API_request.py
+++ tests/test_gmaps_API_request.py
@@ -1,23 +1,28 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
+""" Set TestGmapsApiRequest class.
+
+TestGmapsApiRequest class set a mock of 'get' method from gmaps_API_request.py.
+
+"""
+
 import app.utils.gmaps_API_request as gmaps
 
 
 class TestGmapsApiRequest:
-    gmaps_API_request = gmaps.GmapsApiRequest('piscine Foix')
+    """ Set TestGmapsApiRequest class. """
 
-    def test_get_position_info(self, monkeypatch):
+    def test_gmaps_api_request(self, monkeypatch):
+        """ Set test_get_position_info() method. """
         results = {
             "results": [
                 {
-                    'address_components': {
-                        'formatted_address': '1B Avenue du Général de Gaulle, 09000 Foix, France',
-                        'geometry': {
-                            'location': {
-                                'lat': 42.9600983,
-                                'lng': 1.609331
-                            }
+                    'formatted_address': '1B Avenue du Général de Gaulle, 09000 Foix, France',
+                    'geometry': {
+                        'location': {
+                            'lat': 42.9600983,
+                            'lng': 1.609331
                         }
                     }
                 }
@@ -25,11 +30,23 @@ class TestGmapsApiRequest:
         }
 
         class MockResponse:
+            """ Set MockResponse class. """
             def json(self):
+                """ Set json() method. """
                 return results
 
+            def status_code(self):
+                """ Set status_code() method. """
+                return 200
+
         def mockreturn(url, params):
+            """ Set mockreturn() method. """
             return MockResponse()
 
         monkeypatch.setattr(gmaps, 'get', mockreturn)
-        assert self.gmaps_API_request._get_position_info('piscine Foix') == results["results"][0]
+
+        gmaps_API_request = gmaps.GmapsApiRequest('piscine Foix')
+
+        assert gmaps_API_request.address == results["results"][0]['formatted_address']
+        assert gmaps_API_request.lat == results["results"][0]['geometry']['location']['lat']
+        assert gmaps_API_request.lng == results["results"][0]['geometry']['location']['lng']
