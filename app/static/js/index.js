254ff79b3ff6d2086431ff64e02ba3b4af2065db
Laure <lauremenaut@yahoo.fr>
2018-07-16 13:04:37 -0400

    style : minor improvements

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,3 +1,4 @@
+// displayMap() is a function displaying a map from Google Maps.
 function displayMap(lat, lng, zoom) {
         var place = {lat: lat, lng: lng};
         var map = new google.maps.Map(document.getElementById('map'), {
@@ -10,81 +11,94 @@ function displayMap(lat, lng, zoom) {
         });
       }
 
+// initMap() function is called in the script section at the end of
+// index.html template as a callback method
 function initMap() {
         displayMap(0, 0, 2);
       }
 
+// locate() function send 'query' through '/locate' route to get back a
+// json object containing 'localisation' data
 function locate(query, dialogue_area) {
-    // Ajout de l'animation pendant que "GrandPy réfléchit"
-    $(dialogue_area).append("<img src='../static/img/loading_mini.gif' alt='loading' class='loading' />");
+    // Display loading animation while process is on going
+    $(dialogue_area).append("<img src='../static/img/loading_mini.gif' \
+alt='loading' class='loading' />");
 
     $.post('/locate', {
         query: query
     }).done(function(response) {
-        // Retrait de l'animation
+        // Remove loading animation
         $('.loading').remove();
 
         error = response['localisation'][0];
         message = response['localisation'][1];
 
         if (error) {
+            // Display failure message
             $(dialogue_area).append(message + '<br>');
+            // Force the scrollbar to go down
             $(dialogue_area).scrollTop(100000);
         } else {
 
-            // Affichage de l'adresse
+            // Display success message and address
             address = response['localisation'][2];
             $(dialogue_area).append(message + address + '.<br><br>');
 
-            // Insertion de la carte
+            // Display map
             lat = response['localisation'][3];
             lng = response['localisation'][4];
             displayMap(lat, lng, 17);
 
-            // Affichage de l'anecdote
+            // Display more information about the place
             summary_message = response['localisation'][5];
             summary = response['localisation'][6];
             if (summary) {
+                // Display success message and summary
                 $(dialogue_area).append(summary_message + summary);
             } else {
+                // Display failure message
                 $(dialogue_area).append(summary_message);
             }
 
-            end_message = response['localisation'][7];
-
-            $(dialogue_area).append('<br><br>' + end_message + '<br>');
-
+            // Display a message suggesting to ask a new question
+            next_question_message = response['localisation'][7];
+            $(dialogue_area).append(
+                '<br><br>' + next_question_message + '<br>');
             $(dialogue_area).scrollTop(100000);
         }
 
     }).fail(function() {
-        // Retrait de l'animation
+        // Remove loading animation
         $('.loading').remove();
 
-        $(dialogue_area).append('GrandPy : Oh, désolé ! J\'ai un problème d\'accès à ma mémoire ...<br>');
-
+        // Display failure message
+        $(dialogue_area).append('GrandPy : Oh, désolé ! J\'ai un problème \
+d\'accès à ma mémoire ...<br>');
         $(dialogue_area).scrollTop(100000);
     });
 }
 
 $(function() {
     document.addEventListener("keypress", function(e) {
-        var form = document.querySelector("form"); // Champ de saisie
-        var query = form.elements[1].value; // Contenu du champ de saisie (= saisie de l'utilisateur)
+        var form = document.querySelector("form");
+        var query = form.elements[1].value;
+        // 'query' is the value of user query field
 
         if (e.key == "Enter") {
-            // On affiche la saisie de l'utilisateur dans la zone de dialogue
+            // Display user input in dialogue area
             var divElt = document.createElement("div");
             divElt.textContent = 'Vous : ' + query;
             $('#dialogue_area').append('<br>');
             $('#dialogue_area').append(divElt);
             $('#dialogue_area').append('<br>');
 
+            // Call locate() function to display GrandPy answer
             locate(
                 query,
                 '#dialogue_area'
                 );
 
+            // Clear the input field
             form.elements[1].value = "";
         };
     });
