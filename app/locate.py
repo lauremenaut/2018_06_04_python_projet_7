b384387edc95c8bbbe3bc1d4f57d3c2fb11f791c
Laure <lauremenaut@yahoo.fr>
2018-07-20 10:48:14 -0400

    refactor : move _return_infos() method out of locate() method

--- app/locate.py
+++ app/locate.py
@@ -18,6 +18,47 @@ from app.utils.messages import (ADDRESS_SUCCESS_MESSAGES,
 from app.exceptions import ParserError, GmapsApiError, MediaWikiApiError
 
 
+def _return_infos(error=False,
+                  message=random.choice(ADDRESS_SUCCESS_MESSAGES),
+                  address=None,
+                  lat=None,
+                  lng=None,
+                  summary_message=random.choice(SUMMARY_SUCCESS_MESSAGES),
+                  summary=None,
+                  next_question_message=random.choice(
+                      NEXT_QUESTION_MESSAGES)):
+    """ Set _return_infos() private method.
+
+    Receive 8 parameters :
+    - error : a boolean set to False by default. Switched to True if no
+    found address.
+    - message : a string randomly chosen in messages list.
+    - address : a string set to None by default. Containing the address
+    of the searched place if previously returned by Google Maps API.
+    - lat : a floating number set to None by default. Containing the
+    latitude of the searched place if previously returned by Google Maps
+    API.
+    - lng : a floating number set to None by default. Containing the
+    longitude of the searched place if previously returned by Google
+    Maps API.
+    - summary_message : a string randomly chosen in messages list.
+    - summary : a string set to None by default. Containing informative
+    text about the searched place if previously returned by MediaWiki
+    API.
+    - next_question_message : a string randomly chosen in messages list.
+    Return a tuple containing these 8 variables.
+
+    """
+    # 'error' variable is True only if no address is returned (in parser
+    # failure & address failure cases)
+    if error:
+        summary_message = None
+        next_question_message = None
+
+    return (error, message, address, lat, lng,
+            summary_message, summary, next_question_message)
+
+
 def locate(query):
     """ Set locate() function.
 
@@ -26,48 +67,6 @@ def locate(query):
     Return a tuple of 8 variables via return_infos() method.
 
     """
-    # 'error' variable is True only if no address is returned (in parser
-    # failure & address failure cases)
-    def return_infos(error=False,
-                     message=random.choice(ADDRESS_SUCCESS_MESSAGES),
-                     address=None,
-                     lat=None,
-                     lng=None,
-                     summary_message=random.choice(SUMMARY_SUCCESS_MESSAGES),
-                     summary=None,
-                     next_question_message=random.choice(
-                         NEXT_QUESTION_MESSAGES)):
-        """ Set return_infos() method.
-
-        Receive 8 parameters :
-        - error : a boolean set to False by default. Switched to True if
-        no address is found.
-        - message : a string randomly chosen in messages list.
-        - address : a string set to None by default. Containing the
-        address of the searched place if previously found and sent back
-        by Google Maps API.
-        - lat : a floating number set to None by default. Containing the
-        latitude of the searched place if previously found and sent back
-        by Google Maps API.
-        - lng : a floating number set to None by default. Containing the
-        longitude of the searched place if previously found and sent back
-        by Google Maps API.
-        - summary_message : a string randomly chosen in messages list.
-        - summary : a string set to None by default. Containing
-        informative text about the searched place if previously found
-        and sent back by MediaWiki API.
-        - next_question_message : a string randomly chosen in messages
-        list.
-        Return a tuple containing these 8 variables.
-
-        """
-        if error:
-            summary_message = None
-            next_question_message = None
-
-        return (error, message, address, lat, lng,
-                summary_message, summary, next_question_message)
-
     try:
         # Extract relevant words of user query.
         parser = Parser(query)
@@ -81,9 +80,9 @@ def locate(query):
         logging.warning("ParserError : %s", error)
         # If no relevant words found, error is True. Neither address,
         # nor summary are returned. End of process.
-        return return_infos(error=True,
-                            message=random.choice(
-                                PARSER_FAILURE_MESSAGES))
+        return _return_infos(error=True,
+                             message=random.choice(
+                                 PARSER_FAILURE_MESSAGES))
 
     try:
         # Ask data to Google Maps Geocoding API.
@@ -99,8 +98,8 @@ API : %s, %s", lat, lng)
         # If there is no data returned from Google Maps Geocoding API,
         # then error becomes true. Neither address, nor summary are
         # returned. End of process.
-        return return_infos(error=True,
-                            message=random.choice(ADDRESS_FAILURE_MESSAGES))
+        return _return_infos(error=True,
+                             message=random.choice(ADDRESS_FAILURE_MESSAGES))
 
     try:
         # Ask data to MediaWiki API.
@@ -111,11 +110,11 @@ API : %s, %s", lat, lng)
         logging.warning("MediaWikiError : %s", error)
         # If there is no data returned from MediaWiki API, then only
         # Google Maps data are returned.
-        return return_infos(address=address, lat=lat, lng=lng,
-                            summary_message=random.choice(
-                                SUMMARY_FAILURE_MESSAGES))
+        return _return_infos(address=address, lat=lat, lng=lng,
+                             summary_message=random.choice(
+                                 SUMMARY_FAILURE_MESSAGES))
 
     # If Parser, GmapsApiRequest & MediaWikiApiRequest return data, then
     # all data are returned.
-    return return_infos(address=address, lat=lat, lng=lng,
-                        summary=summary)
+    return _return_infos(address=address, lat=lat, lng=lng,
+                         summary=summary)
